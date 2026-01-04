ae9d30995eb9c5a73f5ff8de709b1ef4f893bc39
HallgrimurICE <144339262+HallgrimurICE@users.noreply.github.com>
2026-01-04 21:33:19 +0000

    Add negotiation deal types

---
+++ src/dip_tom/negotiation/__init__.py
@@ -0,0 +1,19 @@
+"""Negotiation deals and enforcement helpers."""
+
+from dip_tom.negotiation.deal import (
+    Deal,
+    NoEnterDeal,
+    PeaceDeal,
+    SupportDeal,
+    UnitRef,
+    Violation,
+)
+
+__all__ = [
+    "Deal",
+    "NoEnterDeal",
+    "PeaceDeal",
+    "SupportDeal",
+    "UnitRef",
+    "Violation",
+]
